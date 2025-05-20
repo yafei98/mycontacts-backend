96b52ee46ed5d848265c354a3a0f1c16492aa056
yafei98 <yafei.dev@gmail.com>
2025-05-20 20:31:22 +0100

    [0520]Build-in Middleware for POST Request Body: express.json() Express - Throw Error Error Handling Middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,12 @@ const getContacts = (req, res) => {
 //@route Post /api/contact
 //@access public 
 const createContact = (req, res) => {
+    console.log("The request body is:", req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory.")
+    }
     res.status(200).json({ message: "Create Contact" })
 }
 
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
+app.use(express.json()); // Provide a passer which will help us to pass the data stream that we receive from the client on the server side
 app.use("/api/contacts", require("./routes/contactRoutes"))
-
+app.use(errorHandler); //Note to place it at the end of all app.use()
 
 app.listen(port, () => {
     console.log(`Server running on the port ${port}`)
