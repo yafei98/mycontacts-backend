52db18f588ecf09b4a03f9718af04425b3b31e29
yafei98 <yafei.dev@gmail.com>
2025-05-20 19:34:13 +0100

    [0520]Express Router & Contacts CRUD Route Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
+
+app.listen(port, () => {
+    console.log(`Server running on the port ${port}`)
+})
