89ee898d777bc8d686baf64edb7bc0d56644e1e0
yafei98 <yafei.dev@gmail.com>
2025-05-20 22:08:04 +0100

    [0520][FixBug]Connect Express App to MongoDB Database

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const app = express();
-connectDb();
 require('dotenv').config();
+connectDb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json()); // Provide a passer which will help us to pass the data stream that we receive from the client on the server side
